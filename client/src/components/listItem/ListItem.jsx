8a821e9517ef85011ee39b6fadd58fe659708803
vijay meena <jmeena125@gmail.com>
2022-12-07 09:25:53 +0530

    changes in listItem.jsx

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -12,7 +12,7 @@ import { Link } from "react-router-dom";
 export default function ListItem({ index, item }) {
   const [isHovered, setIsHovered] = useState(false);
   const [movie, setMovie] = useState({});
-  axios = axios.create({ baseURL: process.env.API_URL });
+  const axiosInstance = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
   useEffect(() => {
     const getMovie = async () => {
